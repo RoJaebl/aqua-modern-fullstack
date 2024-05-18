df1308ea1f834d7670fccdc0a79c3555da8f50c4
RoJaebl <qgs0924@gmail.com>
2024-05-18 13:26:36 +0900

    :hammer: 사용자 로그아웃 구현

--- src/middlewares/locals.middleware.ts
+++ src/middlewares/locals.middleware.ts
@@ -1,12 +1,17 @@
 import { RequestHandler } from "express";
-import { AppResponse } from "../shared/types";
+import { AppRequest, AppResponse } from "../shared/types";
 
 const localsMiddleware: RequestHandler = (req, res, next) => {
   const { locals } = res as AppResponse;
+  const {
+    session: { loggedIn, user },
+  } = req as AppRequest;
   locals.siteName = "Aqua Modern Fullstack";
+  locals.loggedIn = Boolean(loggedIn);
+  locals.user = user;
+
   delete locals.formData;
   delete locals.error;
-
   next();
 };
 
